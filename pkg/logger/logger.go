cd426e437768438101d833d6e1e0a698fb0f5a00
neutral-jungle-creep <arrrseny11@gmail.com>
2023-03-04 13:45:30 +0300

    add logger.go

---
+++ pkg/logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path"
+	"runtime"
+)
+
+var e *logrus.Entry
+
+type logger struct {
+	*logrus.Entry
+}
+
+func NewLogger(e *logrus.Entry) *logger {
+	return &logger{e}
+}
+
+func init() {
+	log := logrus.New()
+	log.SetReportCaller(true)
+
+	log.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			fileName := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", fileName, frame.Line)
+		},
+		FullTimestamp: true,
+	}
+
+	file, err := os.OpenFile("logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(io.MultiWriter(os.Stdout, file))
+
+	e = logrus.NewEntry(log)
+}
