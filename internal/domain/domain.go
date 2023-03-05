1229ad027a5382c2bd438a60dc8368b97dd7c108
neutral-jungle-creep <arrrseny11@gmail.com>
2023-03-05 19:05:52 +0300

    edit domain.go

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,29 +1,27 @@
 package domain
 
 type UserRequest struct {
-	ClientFirstName   string `json:"clientFirstName" binding:"required" validate:"alpha,max=50"`
-	ClientLastName    string `json:"clientLastName" binding:"required" validate:"alpha,max=50"`
-	ClientMiddleName  string `json:"clientMiddleName" validate:"alpha,max=50"`
-	ClientCompany     string `json:"clientCompany" validate:"max=100"`
-	ClientPhoneNumber string `json:"clientPhoneNumber" validate:"e164"`
-	ClientEmail       string `json:"clientEmail" validate:"email"`
+	ClientFirstName   string `json:"clientFirstName" binding:"required" validate:"alphaunicode,max=50"`
+	ClientLastName    string `json:"clientLastName" binding:"required" validate:"alphaunicode,max=50"`
+	ClientMiddleName  string `json:"clientMiddleName" binding:"required" validate:"alphaunicode,max=50"`
+	ClientCompany     string `json:"clientCompany" binding:"required" validate:"max=100"`
+	ClientPhoneNumber string `json:"clientPhoneNumber" binding:"required" validate:"e164"`
+	ClientEmail       string `json:"clientEmail" binding:"required" validate:"email"`
 
-	ProviderFirstName   string `json:"providerFirstName" validate:"alpha,max=50"`
-	ProviderLastName    string `json:"providerLastName" validate:"alpha,max=50"`
-	ProviderMiddleName  string `json:"providerMiddleName" validate:"alpha,max=50"`
-	ProviderCompany     string `json:"providerCompany" validate:"max=100"`
-	ProviderPhoneNumber string `json:"providerPhoneNumber" validate:"e164"`
-	ProviderEmail       string `json:"providerEmail" validate:"email"`
+	ProviderFirstName   string `json:"providerFirstName" binding:"required" validate:"alphaunicode,max=50"`
+	ProviderLastName    string `json:"providerLastName" binding:"required" validate:"alphaunicode,max=50"`
+	ProviderMiddleName  string `json:"providerMiddleName" binding:"required" validate:"alphaunicode,max=50"`
+	ProviderCompany     string `json:"providerCompany" binding:"required" validate:"max=100"`
+	ProviderPhoneNumber string `json:"providerPhoneNumber" binding:"required" validate:"e164"`
+	ProviderEmail       string `json:"providerEmail" binding:"required" validate:"email"`
 
 	Items []struct {
-		VendorCode string  `json:"vendorCode" validate:"alphanum,max=30"`
-		Price      float32 `json:"price" validate:""`
-		Name       string  `json:"name" validate:"required"`
-		Quantity   uint    `json:"quantity"`
+		VendorCode string  `json:"vendorCode" binding:"required" validate:"alphanum,max=30"`
+		Price      float32 `json:"price" binding:"required"`
+		Name       string  `json:"name" binding:"required" validate:"required"`
+		Quantity   uint    `json:"quantity" binding:"required"`
 	} `json:"items"`
 }
-
-// TODO: add required to all values of struct UserRequest
 
 type Response struct {
 	FileName string `json:"fileName"`
