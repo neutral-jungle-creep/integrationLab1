7d06793e6d2a82fffe51b5a33447fa595b91435d
neutral-jungle-creep <arrrseny11@gmail.com>
2023-03-11 00:01:04 +0300

    add validate form to main.js & error label to index.html

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,35 +2,59 @@ const form = document.getElementById("form")
 const urlGetDoc = "/api/v1/doc/get"
 const urlDownloadDoc = "/api/v1/doc/download/"
 
-form.addEventListener("submit", (event)=> {
-    event.preventDefault()
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (validation(form)) {
+        document.querySelector("label").textContent = "";
+        newDocumentRequest(event);
+    } else {
+        document.querySelector("label").textContent = "Необходимо заполнить все поля!";
+    }
+})
+
+function validation(form) {
+    let validRes = true;
+
+    form.querySelectorAll("input").forEach(input => {
+        if (input.value === "") {
+            input.classList.add("error");
+            validRes = false;
+        } else {
+            input.classList.remove("error");
+        }
+    })
+
+    return validRes;
+}
+
+function newDocumentRequest(event) {
+    event.preventDefault();
     const formData = new FormData(event.target);
     const fData = Object.fromEntries(formData.entries());
-    console.log(fData.clientFullName)
+    console.log(fData.clientFullName);
 
     const body = {
-        clientFullName: fData.clientFullName,
-        clientPhoneNumber: fData.clientPhoneNumber,
-        clientEmail: fData.clientEmail
+        clientFullName: fData.clientFullName, clientPhoneNumber: fData.clientPhoneNumber, clientEmail: fData.clientEmail
     }
 
     async function sendFormData() {
         try {
             const response = await fetch(urlGetDoc, {
-                method: "POST",
-                body: JSON.stringify(body)
+                method: "POST", body: JSON.stringify(body)
             })
             if (response.status === 200) {
                 let res = await response.json()
-                downloadFile(res.fileName)
-                console.log(res.fileName)
+                downloadFile(res.fileName);
+                console.log(res.fileName);
             }
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
+
     sendFormData()
-})
+}
 
 async function downloadFile(filename) {
     const response = await fetch(urlDownloadDoc + filename);
