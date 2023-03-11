028b6ca86f1b344d959e6c939297a3dd482f77e1
neutral-jungle-creep <arrrseny11@gmail.com>
2023-03-11 20:40:48 +0300

    add func getFileNameWithExtension to writeDocument.go & TemplateFile to domain.go

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,26 +1,32 @@
 package domain
 
 type UserRequest struct {
-	ClientFirstName   string `json:"clientFirstName" binding:"required" validate:"alphaunicode,max=50"`
-	ClientLastName    string `json:"clientLastName" binding:"required" validate:"alphaunicode,max=50"`
-	ClientMiddleName  string `json:"clientMiddleName" binding:"required" validate:"alphaunicode,max=50"`
-	ClientCompany     string `json:"clientCompany" binding:"required" validate:"max=100"`
-	ClientPhoneNumber string `json:"clientPhoneNumber" binding:"required" validate:"e164"`
-	ClientEmail       string `json:"clientEmail" binding:"required" validate:"email"`
+	TemplateFile string `json:"templateFile" binding:"required"`
 
-	ProviderFirstName   string `json:"providerFirstName" binding:"required" validate:"alphaunicode,max=50"`
-	ProviderLastName    string `json:"providerLastName" binding:"required" validate:"alphaunicode,max=50"`
-	ProviderMiddleName  string `json:"providerMiddleName" binding:"required" validate:"alphaunicode,max=50"`
-	ProviderCompany     string `json:"providerCompany" binding:"required" validate:"max=100"`
-	ProviderPhoneNumber string `json:"providerPhoneNumber" binding:"required" validate:"e164"`
-	ProviderEmail       string `json:"providerEmail" binding:"required" validate:"email"`
+	ClientFullName        string `json:"clientFullName" binding:"required" validate:"max=250"`
+	ClientPhoneNumber     string `json:"clientPhoneNumber" binding:"required" validate:"e164"`
+	ClientEmail           string `json:"clientEmail" binding:"required" validate:"email"`
+	ClientCompany         string `json:"clientCompany" binding:"required" validate:"max=250"`
+	ClientCompanyFullName string `json:"clientCompanyFullName" binding:"required" validate:"max=250"`
+	ClientCompanyInnKpp   string `json:"clientCompanyInnKpp" binding:"required" validate:"min=22,max=24"`
+	ClientCompanyAddress  string `json:"clientCompanyAddress" binding:"required" validate:"max=250"`
+
+	ProviderFullName        string `json:"providerFullName" binding:"required" validate:"max=50"`
+	ProviderPhoneNumber     string `json:"providerPhoneNumber" binding:"required" validate:"e164"`
+	ProviderEmail           string `json:"providerEmail" binding:"required" validate:"email"`
+	ProviderCompany         string `json:"providerCompany" binding:"required" validate:"max=250"`
+	ProviderCompanyFullName string `json:"providerCompanyFullName" binding:"required" validate:"max=250"`
+	ProviderCompanyInnKpp   string `json:"providerCompanyInnKpp" binding:"required" validate:"min=22,max=24"`
+	ProviderCompanyAddress  string `json:"providerCompanyAddress" binding:"required" validate:"max=250"`
+
+	DeliveryAddress string `json:"deliveryAddress" binding:"required" validate:"max=250"`
 
 	Items []struct {
 		VendorCode string  `json:"vendorCode" binding:"required" validate:"alphanum,max=30"`
-		Price      float32 `json:"price" binding:"required"`
-		Name       string  `json:"name" binding:"required" validate:"required"`
+		Name       string  `json:"itemName" binding:"required" validate:"required"`
 		Quantity   uint    `json:"quantity" binding:"required"`
-	} `json:"items"`
+		Price      float32 `json:"price" binding:"required"`
+	} `json:"items" binding:"required"`
 }
 
 type Response struct {
